74a901c883876b692db9d6f128d5e1f0778b351b
amarotxt <amaro.txt@gmail.com>
2017-11-11 20:00:46 -0200

    Audio com animacao

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound[] sounds;
+	// Use this for initialization
+	void Aweke () {
+//		foreach(Sound sound in sounds){
+//			sound.source = gameObject.AddComponent<AudioSource> ();
+//			sound.source.clip = sound.clip;
+//			sound.source.volume = sound.volume;
+//			sound.source.pitch = sound.pitch;
+//		}
+	}
+	
+	public void Play (string name){
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+//		s.source.Play ();
+	}
+}
